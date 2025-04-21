ea9dfea783617d4f4dadc038cd10b1d4a4fa8e86
kuroroLuciferPhantom <91409971+kuroroLuciferPhantom@users.noreply.github.com>
2025-04-21 14:37:36 +0200

    Ajout du fichier principal src/main.js

---
+++ src/main.js
@@ -0,0 +1,13 @@
+/**
+ * Fichier principal du projet
+ */
+
+// Point d'entrée du projet
+function main() {
+  console.log('Projet initialisé avec succès!');
+  
+  // Votre code ici
+}
+
+// Exécution de la fonction principale
+main();
