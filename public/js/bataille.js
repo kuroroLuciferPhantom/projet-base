6ad01597452f1c025da8c7f11bb01c9a7ffebda1
kuroroLuciferPhantom <91409971+kuroroLuciferPhantom@users.noreply.github.com>
2025-05-05 13:31:24 +0200

    Ajout du fichier d'import des modules pour la bataille

---
+++ public/js/bataille.js
@@ -0,0 +1,11 @@
+/**
+ * Script principal pour la fonctionnalité Bataille
+ * Importe tous les modules nécessaires
+ */
+
+// Chargement des modules dans l'ordre de dépendance
+document.write('<script src="/js/bataille-ui.js"></script>');
+document.write('<script src="/js/bataille-stats.js"></script>');
+document.write('<script src="/js/bataille-battle.js"></script>');
+document.write('<script src="/js/bataille-decks.js"></script>');
+document.write('<script src="/js/bataille-main.js"></script>');
